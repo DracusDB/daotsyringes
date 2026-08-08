e15bc33828d5768791fefec9618246f757ecddfd
Michael <giordanomichael2@gmail.com>
2026-08-08 15:03:43 -0400

    Initial commit

---
+++ src/main/java/net/dracus/daotsyringes/DAOTSyringesClient.java
@@ -0,0 +1,10 @@
+package net.dracus.daotsyringes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DAOTSyringesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dracus/daotsyringes/DAOTSyringesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dracus.daotsyringes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DAOTSyringesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
